9097ae8007758cbb61695715929c22b3bf47d7e8
Ordin <18818+lukelove@users.noreply.github.com>
2022-04-09 18:49:12 +0800

    Allow the port to be set with an ENV['MY_RAY_PORT']

--- lib/ray.rb
+++ lib/ray.rb
@@ -203,7 +203,7 @@ module Ray
 end
 
 def ray(*args)
-  settings = {host: (ENV['MY_RAY_URL'] || "http://localhost"), port: 23517}
+  settings = {host: (ENV['MY_RAY_URL'] || "http://localhost"), port: (ENV['MY_RAY_PORT'] || 23517)}
 
   Ray::Ray.new(settings).send(*args)
 end
